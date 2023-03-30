b19238b0a6716adf2765ba0f18aff32f0836b99c
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-03-30 12:08:52 +0600

    calculates the volume of a cube.

---
+++ cube.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  int num1, result;
+  cout << "Enter the length of Cube: ";
+  cin >> num1;
+
+  result = num1 * num1 * num1;
+
+  cout << "The Area of The Cube is: " << result << endl;
+
+  return 0;
+}
